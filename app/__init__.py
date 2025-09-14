9c581d4c09fb8b874da9c9d436a59d5aaa68ca42
kk2215 <kaitokk2002@gmail.com>
2025-09-14 09:10:07 +0900

    はが

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,18 @@ import os
 from flask import Flask
 from .models import db
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# ★ 2. LoginManagerのインスタンスを作成
+login_manager = LoginManager()
+# ★ 3. ログインしていないユーザーがアクセスした場合にリダイレクトする先を指定
+login_manager.login_view = 'main.login' 
+# ↑ 'main.login' の部分は、あなたのログインページのルート関数名に合わせてください
+
+# このファイルで定義しているdbやmigrateと同じように追加します
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -27,9 +39,17 @@ def create_app():
     db.init_app(app)
     migrate = Migrate() # <-- Migrateインスタンスを先に作成
     migrate.init_app(app, db) # <-- 正しい変数名 "migrate" を使用
-
+    
+    login_manager.init_app(app)
+    
     # ルート（Blueprint）の登録
     from . import routes
     app.register_blueprint(routes.bp)
 
     return app
+
+from .models import User # Userモデルをインポート
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
